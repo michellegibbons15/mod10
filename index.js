dfcf50949c04caa27904fb105c324da0165981e4
MichelleGibbons15 <mcgibbons615@gmail>
2025-03-06 08:49:36 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+io.on("connection", (socket) => {
+  io.emit("connection", "a user connected");
+});
+server.listen(3000, () => {
+  console.log("listening on http://localhost:3000");
+});
+
